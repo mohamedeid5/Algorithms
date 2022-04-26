c04aa656f32778b3a665fb0b7272c1dad479f016
mohamed eid <meid368@gmail.com>
2022-04-26 14:58:31 +0200

    quick-sort

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,35 @@
-def power(x, y):
-    if y == 1:
-        return x
+# main
+
+def partition(arr, low, high):
+    i = (low - 1)
+    pivot = arr[high]
+    print(arr)
     
-    result = power(x, int(y/2))
+    for j in range(low, high):
+        # If current element is smaller than or
+        # equal to pivot
+        if arr[j] <= pivot:
+            i=i+1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    print(arr)
+    return (i+1)
+
+def quickSort(arr, low, high):
+    if len(arr) == 1:
+        return arr
+    if low < high:
+        # pi is partitioning index, arr[p] is now
+        # at right place
+        pi = partition(arr, low, high)
+        
+        # Separately sort elements before
+        # partition and after partition
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
+    print(arr)
     
-    if int(y % 2) == 0:
-        return result * result
-    else:
-        return result * result * x
     
-print(power(2,5))
+arr = [10, 7, 8, 9, 1, 5]    
+n = len(arr)
+quickSort(arr, 0, n-1)
